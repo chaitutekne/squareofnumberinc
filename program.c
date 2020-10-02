ed8ff95e8f64625ab040ea4297e6df6527044267
chaitutekne <72239009+chaitutekne@users.noreply.github.com>
2020-10-02 20:16:05 +0530

    program for square
    c program to accept a number and display its square

---
+++ program.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        clrscr();
+        int x,y;
+        printf("Enter a number: ");
+        scanf("%d",&x);
+        y=x*x;
+        printf("The square of %d is %d",x,y);
+        getch();
+}
